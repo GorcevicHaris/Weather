a628a3d4703f1fe7106269fde94c0b8930e981c2
macbook <harisgorcevic1000@gmail.com>
2023-12-05 21:08:44 +0100

    rad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { func } from "prop-types";
+import "./App.css";
+import { IoSearchOutline } from "react-icons/io5";
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const api_key = "b95eb0fe5bb1f890189a08ceded58688";
+  function getData() {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=tutin&units=Metric&appid=${api_key}`
+      )
+      .then((response) => setData(response.data));
+  }
+  useEffect(() => {
+    getData();
+  }, [search]);
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="top">
+          <input></input>
+          <button>
+            <IoSearchOutline fontSize={25} />
+          </button>
+        </div>
+        <div className="mid"></div>
+        <div className="bot"></div>
+      </div>
     </div>
   );
 }
