86d187436eb101201697b29a6abdb6ce8e23cb45
macbook <harisgorcevic1000@gmail.com>
2024-01-10 20:07:33 +0100

    work

--- src/App.js
+++ src/App.js
@@ -3,31 +3,42 @@ import "./App.css";
 import { IoSearchOutline } from "react-icons/io5";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { IoSunny } from "react-icons/io5";
+
 function App() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("tutin");
   const [data, setData] = useState([]);
+  const [dataTemp, setDataTemp] = useState([]);
   const api_key = "b95eb0fe5bb1f890189a08ceded58688";
   function getData() {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=tutin&units=Metric&appid=${api_key}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=Metric&appid=${api_key}`
       )
-      .then((response) => setData(response.data));
+      .then((response) => {
+        setData((prev) => [...prev, response.data]);
+        setDataTemp(response.data.main);
+      })
+      .catch(() => {
+        console.log("doslo je do greske niste ukucali ispravno grad");
+      });
   }
-  useEffect(() => {
-    getData();
-  }, [search]);
-  console.log(data);
+  useEffect(() => {}, [search]);
+  console.log(data, "data");
+  console.log(dataTemp, "datatemp");
   return (
     <div className="container">
       <div className="card">
         <div className="top">
-          <input></input>
+          <input onChange={(el) => setSearch(el.target.value)}></input>
           <button>
-            <IoSearchOutline fontSize={25} />
+            <IoSearchOutline fontSize={25} onClick={getData} />
           </button>
         </div>
-        <div className="mid"></div>
+        <div className="mid">
+          <IoSunny color="yellow" fontSize={200} />
+          <h1>{dataTemp.temp}</h1>
+        </div>
         <div className="bot"></div>
       </div>
     </div>
