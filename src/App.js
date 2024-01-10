20f64ea8ded983ebd48c94cb2e558dd3826ef550
macbook <harisgorcevic1000@gmail.com>
2024-01-10 22:26:22 +0100

    Rad

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import { IoSunny } from "react-icons/io5";
 import { BsMoisture } from "react-icons/bs";
 import { PiWind } from "react-icons/pi";
 
+// ... (your existing imports)
+
 function App() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
   const [dataTemp, setDataTemp] = useState([]);
   const [fasling, setFalsing] = useState(false);
   const api_key = "b95eb0fe5bb1f890189a08ceded58688";
+
   function getData() {
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=Metric&appid=${api_key}`
       )
       .then((response) => {
-        setData(response.data);
+        setData(response.data.wind);
         setDataTemp(response.data.main);
         setFalsing(true);
       })
@@ -27,37 +30,52 @@ function App() {
         console.log("doslo je do greske niste ukucali ispravno grad");
       });
   }
-  useEffect(() => {}, [search]);
+
+  useEffect(() => {
+    setFalsing(false);
+  }, [search]);
+
   console.log(data, "data");
   console.log(dataTemp, "datatemp");
+
   return (
     <div className="container">
       <div className="card">
         <div className="top">
-          <input onChange={(el) => setSearch(el.target.value)}></input>
+          <input
+            placeholder="Search"
+            onChange={(el) => setSearch(el.target.value)}
+          ></input>
           <button>
             <IoSearchOutline fontSize={25} onClick={getData} />
           </button>
         </div>
         <div className="mid">
           <IoSunny color="yellow" fontSize={200} />
-          {search.length > 0 && dataTemp.temp ? (
-            <h1>{`${dataTemp.temp}°`}</h1>
+          {fasling && dataTemp.temp ? (
+            <>
+              <h1>{`${dataTemp.temp}°`}</h1>
+              <h1>{search}</h1>
+            </>
           ) : (
             ""
           )}
-          {fasling && search ? <h1>{search}</h1> : ""}
         </div>
         <div className="bot">
           <div className="div2">
-            <BsMoisture fontSize={40} />
-            <PiWind fontSize={50} />
+            <div className="sameDiv">
+              <BsMoisture color="white" fontSize={40} />
+              <h2>{dataTemp.humidity}</h2>
+            </div>
+            <div className="sameDiv">
+              <PiWind color="white" fontSize={50} />
+              <h2>{data.speed}</h2>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
-//
 
 export default App;
